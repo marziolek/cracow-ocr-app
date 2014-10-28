4476afc0e25ce3d216c8711845a3d5f3adc6fe17
zywszy <zywszy@gmail.com>
2014-10-28 21:32:34 +0100

    Upload problems

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,24 @@ class HomeController < ApplicationController
   end
 
   def upload
+    #
+    # Here goes file saving
+    #
+
+    DataFile.save_file(params[:picture])
+    @data = params[:picture]
+
+    @img = @data.read
+    #name = params['datafile']
+    #directory = "images/upload"
+    #path = File.join(directory, name)
+    #File.open(path, "wb") do |f|
+    #  f.write(params[:picture].read)
+    #end
+    #File.write(path, 'asd')
+    #flash[:notice] = "File uploaded!"
+    #
+    # It should run smoothly
+    #
   end
 end
